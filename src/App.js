866154b9868c1f4263d6005b79b42d4c833b7159
borkarSahil <officialsahilborkar@gmail.com>
2024-03-28 11:13:32 +0530

    Job Hunt WebSite with Rapid API

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter, Routes, Route } from "react-router-dom";
+import NavBar from "../src/Components/NavBar/NavBar";
+
+import Home from "./Components/Home/Home";
+import About from "./Components/About/About";
+import Contact from "./Components/Contact/Contact";
+import Footer from "./Components/Footer/Footer";
+import Jobsearch from "./Components/JobSearch.js/Jobsearch";
+import CardDetail from "./Components/CardDetails/CardDetail";
+import Error from "./Components/Error/Error";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+
+      <BrowserRouter>
+      <NavBar />
+        <Routes>
+
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} /> 
+        <Route path="/jobsearch" element={<Jobsearch />} /> 
+        <Route path="/carddetail/:id" element={<CardDetail />} />
+        <Route path="/*" element={<Error />} />
+
+        </Routes>
+      <Footer /> 
+
+      </BrowserRouter>
+
+
+
   );
 }
 
